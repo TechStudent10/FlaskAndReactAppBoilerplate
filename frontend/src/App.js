91d089009a611986dde1b9a246a79958ebf9b3ca
TechStudent11 <76978184+TechStudent11@users.noreply.github.com>
2021-02-05 11:27:00 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,22 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function FetchFromFlask(endpoint, method='POST', body="") {
+  const [state, stateSetter] = useState('');
+  
+  useEffect(() => {
+    fetch(endpoint, {method:method,body:body}).then(res => res.json()).then(data => {
+      stateSetter(data.result);
+    })
+  // eslint-disable-next-line
+  }, []);
+  return state
+}
+
 function App() {
   const [flask, setFlask] = useState('hi');
 
-  function FetchFromFlask(endpoint, state, stateSetter, method='POST', body="") {
-    useEffect(() => {
-      fetch(endpoint, {method:method,body:body}).then(res => res.json()).then(data => {
-        stateSetter(data.result);
-      })
-    // eslint-disable-next-line
-    }, []);
-    return state
-  }
-
-  FetchFromFlask('/hello', flask, setFlask);
+  setFlask(FetchFromFlask('/hello'));
 
   return (
     <div className="App">
